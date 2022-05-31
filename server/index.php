c2b97f069adf7dd964da9e619b375ee96431af86
Wellington Cesar <wcdiaslima@outlook.com.br>
2022-05-31 20:08:47 -0300

    Adicao do servidor PHP

---
+++ server/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Teste</title>
+    </head>
+    <body>
+        <?php
+            echo "De dentro do PHP!!!!"
+        ?>
+    </body>
+</html>
