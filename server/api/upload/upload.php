e31aecce3a7874c690f5b34bee5db32bc542296f
klesleySilvaOliveiraUFU <72235468+klesleySilvaOliveiraUFU@users.noreply.github.com>
2022-08-08 15:50:09 -0300

    Add files via upload

--- server/api/upload/upload.php
+++ server/api/upload/upload.php
@@ -14,22 +14,29 @@
     }
 
     $username = $_GET["username"] ?? "";
-    //$file = $_POST["file"] ?? "";
+    //$file = $_GET["file"] ?? "";
     $nome = $_GET["nome"] ?? "";
-    /*$descricao = $_POST["descricao"] ?? "";
-    $titulo = $_POST["titulo"] ?? "";
+    /*$descricao = $_GET["descricao"] ?? "";
+    $titulo = $_GET["titulo"] ?? "";
     $qtd_downloads = 0;*/
 
     // Checking whether file exists or not
-echo $target_dir . $username . '/' . $nome;
     if (!file_exists($target_dir . $username . '/' . $nome)) {
     
         // Create a new file or direcotry
-        mkdir($target_dir . $username . '/' . $nome, 0777, true);
+        try {
+            mkdir($target_dir . $username . '/' . $nome, 0777, true);
+        } catch(ErrorException $ex) {
+            echo "Error: " . $ex->getMessage();
+            return;
+        }        
 
+        $target_dir = $target_dir . $username . '/' . $nome;
+        $arquivos[] = $target_dir;
         $arquivos[] =  "Given file path successfully created";
     }
     else {
+        $arquivos[] = $target_dir;
         $arquivos[] =  "The Given file path already exists";
     }
 
@@ -37,19 +44,16 @@ echo $target_dir . $username . '/' . $nome;
         fopen($target_dir . $username . '/' . basename($file),mode)
     }
     else{
-
     }*/
 
     /*$sql_time = <<<SQL
                 SELECT * FROM arquivo
                 ORDER BY data DESC;
                 SQL;
-
     $sql_downloads = <<<SQL
                     SELECT * FROM arquivo
                     ORDER BY qtd_downloads DESC;
                     SQL;
-
     $sql_votes = <<<SQL
                 SELECT *
                 FROM avalia AV JOIN (arquivo AR JOIN usuario US ON 
@@ -57,28 +61,21 @@ echo $target_dir . $username . '/' . $nome;
                     AR.identificador = AV.identificador_criador
                 ORDER BY nota DESC;
                 SQL;
-
     if($type == "time"){
         $stmt = $pdo->query($sql_time);
-
         while($row = $stmt->fetch()){
             $arquivos[] = $row;
         }
-
     } else if($type == "downloads"){
         $stmt = $pdo->query($sql_downloads);
-
         while($row = $stmt->fetch()){
             $arquivos[] = $row;
         }
-
     } else if($type == "votes"){
         $stmt = $pdo->query($sql_votes);
-
         while($row = $stmt->fetch()){
             $arquivos[] = $row;
         }
-
     } else{
         $arquivos[] = "Input Error!";
     }*/
