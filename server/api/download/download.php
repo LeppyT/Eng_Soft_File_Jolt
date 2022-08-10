71376b96df631d3635a111170376855f4347c877
klesleySilvaOliveiraUFU <72235468+klesleySilvaOliveiraUFU@users.noreply.github.com>
2022-08-09 22:38:28 -0300

    Add files via upload

--- server/api/download/download.php
+++ server/api/download/download.php
@@ -42,6 +42,21 @@
         }
     }
 
+    $attachment_location = $target_dir . "/*";
+    if (file_exists($attachment_location)) {
+
+        header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+        header("Cache-Control: public"); // needed for internet explorer
+        //header("Content-Type: application/zip");
+        header("Content-Transfer-Encoding: Binary");
+        header("Content-Length:".filesize($attachment_location));
+        header("Content-Disposition: attachment;");
+        readfile($attachment_location);
+        die();        
+    } else {
+        die("Error: File not found.");
+    } 
+
     /*if ($username != "" && $file != ""){
         fopen($target_dir . $username . '/' . basename($file),mode)
     }
