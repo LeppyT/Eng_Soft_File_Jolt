dff43687887937ad88e10a1a581966a253362d94
Wellington Cesar <wcdiaslima@outlook.com.br>
2022-08-10 01:08:22 -0300

    .

--- server/api/download/download.php
+++ server/api/download/download.php
@@ -52,7 +52,7 @@
             //header("Content-Type: application/zip");
             header("Content-Transfer-Encoding: Binary");
             header("Content-Length:".filesize($file));
-            header('Content-Disposition: attachment; filename="' . $file . '"');
+            header('Content-Disposition: attachment; filename="' . basename($file) . '"');
             readfile($file);
             die();        
         } else {
